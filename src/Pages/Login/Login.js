bb971deefd63528af2a19fc5e71400a5d4e0139c
youngho052 <qkrdudgh2701@gmail.com>
2021-01-11 20:52:58 +0900

    Add: 로그인페이지 레이아웃 구현

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import LoginInput from '../../Components/LoginInput';
+import './Login.scss';
+import '../../Styles/reset.scss';
 
 class Login extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="Login">
+        <div className="LoginContainer">
+          <div className="LoginLogo">
+            <h1>AGEOSTE</h1>
+          </div>
+          <LoginInput />
+          <div className="LoginButton">
+            <button
+              id="LoginBtn"
+              type="submit" 
+            >
+              로그인
+            </button>
+          </div>
+          <div className="SignUp">
+            <span>처음이신가요?</span>
+            <Link className="SignupPage">회원가입</Link>
+          </div>
+          <div className="Forget">
+            <span>아이디(이메일) 비밀번호를 잊으셨나요?</span>
+            <Link className="ForgetId">아이디(이메일) 비밀번호 찾기</Link>
+          </div>
+        </div>
+      </div>  
+    );
+  }
 }
 
 export default Login;
