94f5b81f95a884580bccf7f61bc2748e6637a00f
JaehyunYoo <nike361@naver.com>
2021-01-12 18:29:03 +0900

    Modify: Branch 명 수정 및 리뷰 반영

---
+++ src/Components/Navbar/Components/NavbarUser.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class NavbarUser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navbarUser: ['fa-map-marker-alt', 'fa-user', 'fa-cart-plus'],
+    };
+  }
+  render() {
+    const { navbarUser } = this.state;
+    return (
+      <ul className='NavbarUser'>
+        {navbarUser.map((list, idx) => {
+          return (
+            <li key={idx}>
+              <Link to='/'>
+                <i className={`far ${list}`} />
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export default NavbarUser;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './Styles/common.scss';
+import './Styles/reset.scss';
+import '@fortawesome/fontawesome-free/js/all.js';
+
 import Routes from './Routes';
 
 ReactDOM.render(
