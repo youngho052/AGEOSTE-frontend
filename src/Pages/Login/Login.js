6d10798d1f8a23b460a2fa819ef77b2fcad839b5
youngho052 <qkrdudgh2701@gmail.com>
2021-01-15 01:09:40 +0900

    Modify: 로그인페이지 레이아웃 수정

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+
 import './Login.scss';
 import '../../Styles/reset.scss';
 
@@ -18,47 +19,51 @@ class Login extends Component {
   render() {
     const {hiddenPw} = this.state;
     const icon = hiddenPw ? "fas fa-eye-slash" : "fas fa-eye";
-
+    
     return (
       <div className="Login">
         <div className="LoginContainer">
-          <div className="LoginLogo">
-            <h1>로그인</h1>
+          <div className="Logincont">
+            <h1>Login</h1>
           </div>
-          <div className="IdForm">
-          <input
-            className="IdInput" 
-            type="text" 
-            placeholder="아이디(이메일)" 
-          />
-        </div>
-        <div className="PwForm">
-          <input
-            className="PwInput"
-            type={hiddenPw ? "password" : "text"}
-            placeholder="비밀번호"
-          />
+          <div className='IdForm'>
+            <i class="far fa-user" id="IdIcon" />
+            <input 
+              type="text"
+              className="IdInput"
+              placeholder="아이디"
+            />
+          </div>
+          <div className='PwForm'>
+            <i class="fas fa-lock" id="PwIcon"/>
+            <input
+              className="PwInput"
+              type={hiddenPw ? "password" : "text"}
+              placeholder="비밀번호"
+            /> 
+          </div>
           <div className="show" onClick={this.showPassword}>
             <i key={icon}>
               <span className={icon} />
             </i>
-          </div>     
-        </div>
+          </div>
+          <div className="Remember">
+            <div className="RememberId">
+              <input 
+                type="checkbox" 
+              />
+              <span>Remember ID</span>
+            </div>
+            <div className="ForgetPw">
+              <Link to='/'><span>Forget Password?</span></Link>
+            </div>
+          </div>
           <div className="LoginButton">
-            <button
-              id="LoginBtn"
-              type="submit" 
-            >
-              로그인
-            </button>
+            <button>Login</button>
           </div>
           <div className="SignUp">
-            <span>처음이신가요?</span>
-            <Link to="/signup" className="SignupPage">회원가입</Link>
-          </div>
-          <div className="Forget">
-            <span>아이디(이메일) 비밀번호를 잊으셨나요?</span>
-            <Link className="ForgetId">아이디(이메일) 비밀번호 찾기</Link>
+            <h1>OR</h1>
+            <Link to='/'><p>Sign up</p></Link>
           </div>
         </div>
       </div>  
