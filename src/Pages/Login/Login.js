952bb5b84ce7ee10353dcfebd4212a38b7813051
youngho052 <qkrdudgh2701@gmail.com>
2021-01-16 14:51:44 +0900

    Modify: 로그인페이지 리뷰 수정

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -12,7 +12,7 @@ class Login extends Component {
     }
   }
 
-  showPassword = () => {
+  CheckPassword = () => {
     this.setState({hiddenPw : !this.state.hiddenPw});
   }
 
@@ -23,10 +23,10 @@ class Login extends Component {
     return (
       <div className="Login">
         <h1>로그인</h1>
-        <div className="LoginContainer"> 
-          <div className="LoginCont">
+        <div className="loginContainer"> 
+          <div className="loginCont">
             <h1>AGEOSTE 통합회원</h1>
-            <div className="LoginForm">
+            <div className="loginForm">
               <label>
                 <i className="far fa-user" id="idIcon"/>
               </label>
@@ -36,7 +36,7 @@ class Login extends Component {
                 placeholder="아이디"
               />
             </div>
-            <div className="PwForm">
+            <div className="pwForm">
               <label className="pwIcon">
                 <i className="fas fa-lock" id="pwIcon"/>
               </label>
@@ -45,35 +45,35 @@ class Login extends Component {
                 className="pwInput input__padding"
                 placeholder="비밀번호"
               />
-              <label className="show" onClick={this.showPassword}>
+              <label className="show" onClick={this.CheckPassword}>
                 <i key={icon}>
                   <span className={icon} />
                 </i>
               </label>
             </div>
             <div className="Remember">
-              <div className="RememberId">
+              <div className="rememberId">
                 <input 
                   type="checkbox" 
                 />
                 <span>아이디저장</span>
               </div>
-              <div className="ForgetPw">
+              <div className="forgetPw">
                 <Link to='/'><span>비밀번호를 잊으셨나요?</span></Link>
               </div>
             </div>
-            <div className="LoginButton">
+            <div className="loginButton">
               <button>로그인</button>
             </div>
           </div>
-          <div className="SignupCont">
-            <div className="SignupForm">
+          <div className="signupCont">
+            <div className="signupForm">
               <h1>아직 AGEOSTE 회원이 아니신가요?</h1>
               <div className="benifit">
                 <span>지금 AGEOSTE에 회원가입하시면</span>
                 <span>다양한 멤버쉽 혜택을 누리실 수 있습니다.</span>
               </div>
-              <img src="images/Membership.png" className="Logo" alt="아거스테 로고" />
+              <img alt="아거스테 로고" src="images/Membership.png" className="Logo" />
               <div className="SignupBtn">
                 <Link to='/signup'><button>회원가입</button></Link>
               </div>
