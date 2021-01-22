3aef87d2dabf7408e0229edb89d03373205b5afc
youngho052 <qkrdudgh2701@gmail.com>
2021-01-22 10:49:14 +0900

    Modify: wip

--- src/Pages/Myprofile/Myprofile.js
+++ src/Pages/Myprofile/Myprofile.js
@@ -28,7 +28,7 @@ class Myprofile extends Component {
   }
 
   componentDidMount() {
-    fetch('/data/data.json')
+    fetch('/data/userData.json')
       .then(response => response.json())
       .then(result => this.setState({gradeList : result}))
     
@@ -126,7 +126,7 @@ class Myprofile extends Component {
                     </button>
                   </div>
                   <p>휴대폰 번호: {userInfo.phone_number}</p>
-                  <p>생일: {userInfo.date_of_birth === null ? '없다' : userInfo.date_of_birth}</p>
+                  <p>생일: {userInfo.date_of_birth === null ? '생일을 등록해주세요.' : userInfo.date_of_birth}</p>
                 </div>
               </div>
               <div className="secondCont">
