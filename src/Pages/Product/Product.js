e00f44524e77a6e124518c518e10782cddb7a5bb
JaehyunYoo <nike361@naver.com>
2021-01-22 11:31:49 +0900

    Add:Product 페이지 구현중

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
-// import ProductList from '../ProductList/ProductList';
+import Footer from '../../Components/Footer/Footer';
+import Navbar from '../../Components/Navbar/Navbar';
+import ProductList from '../../Components/ProductList/ProductList';
+import { PRODUCT } from '../../config/Server';
 import './Product.scss';
 class Product extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      productListItem: [],
+    };
   }
-  componentDidMount = () => {
-    
+  componentDidMount() {
+    fetch(`${PRODUCT}=폴로`)
+      .then((res) => res.json())
+      .then((res) => this.setState({productListItem:res.PRODUCTS_LIST}));
   }
   render() {
+    const {productListItem} =this.state
+    console.log(this.state.productListItem);
     return (
-			<div className="Product">
-        <div className='container'>
-          {/* <ProductList /> */}
+      <>
+        <Navbar />
+        <div className='Product'>
+          <div className='container'>
+          {productListItem.map((item) => {
+            return (
+              <ProductList
+                key={item.id}
+                id={item.id}
+                name={item.name}
+                price={item.price}
+                discount={item.discount_rate}
+                review={item.review_score_avg}
+                thumbnail={item.thumbnail}
+                colorCount={item.color_count}
+              />
+            );
+          })}
+            
+          </div>
         </div>
-      </div>
+        <Footer />
+      </>
     );
   }
 }
--- src/Pages/Signup/Data/Config.js
+++ src/Pages/Signup/Data/Config.js
@@ -1,3 +1,2 @@
 export const SERVER = "http://10.58.57.113:8000";
-
 export const SERVER_SIGNUP = `${SERVER}/user/signup`;
