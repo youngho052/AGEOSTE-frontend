11e16e2013b1f3340b64944c2ce83c96e1e7c46a
JaehyunYoo <nike361@naver.com>
2021-01-22 10:09:33 +0900

    Add:상품리스트 추가 구현중

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-
+// import ProductList from '../ProductList/ProductList';
+import './Product.scss';
 class Product extends Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+  componentDidMount = () => {
+    
+  }
   render() {
     return (
 			<div className="Product">
-        
+        <div className='container'>
+          {/* <ProductList /> */}
+        </div>
       </div>
     );
   }
--- src/Routes.js
+++ src/Routes.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Login from './Pages/Login/Login';
 import Main from './Pages/Main/Main';
-import ProductList from "./Pages/ProductList/ProductList";
-import ProductDetail from './Pages/ProductDetail/ProductDetail';
+
+import Product from './Pages/Product/Product';
 import Myprofile from './Pages/Myprofile/Myprofile';
 import Signup from './Pages/Signup/Signup';
 
@@ -16,17 +16,14 @@ class Routes extends React.Component {
         <Switch>
           <Route exact path='/' component={Main} />
           <Route exact path='/login' component={Login} />
-<<<<<<< HEAD
           <Route exact path='/product/' component={Product} />
-          <Route exact path="/product/detail/:id" component={ProductDetail} />
-=======
+          {/* <Route exact path="/product/detail/:id" component={ProductDetail} /> */}
           <Route exact path='/myprofile' component={Myprofile} />
           {/* <Route exact path='/product' component={Product} /> */}
           <Route exact path='/signup' component={Signup} />
           <Route exact path='/main' component={Main} />
-          <Route exact path='/product_list' component={ProductList} />
-          <Route exact path='/product_detail' component={ProductDetail} />
->>>>>>> main
+          {/* <Route exact path='/product_list' component={ProductList} /> */}
+          {/* <Route exact path='/product_detail' component={ProductDetail} /> */}
         </Switch>
       </Router>
     );
